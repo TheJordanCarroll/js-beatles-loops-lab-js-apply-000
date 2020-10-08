d720c08356faa33f7a845c120c3b8cddbaa3298f
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-08 21:30:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var musicians, instruments;
 
 musicians = ["John Lennon", "Paul McCartney," "Ringo Starr," "George Harrison"];
 
-instruments = [""]
+instruments = ["guitar", ""]
 
 var musicians = []
 var instruments = []
