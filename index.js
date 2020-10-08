602d986874d2cefd5d0de4ef246a7fc1381a72df
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-08 21:25:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ musicians = ["John Lennon", "Paul McCartney," "Ringo Starr," "George Harrison"];
 var musicians = []
 var instruments = []
   
+for (let i = 0; i < selectObject.options.length; i++)
   for 
+  
 function theBeatlesPlay ()
