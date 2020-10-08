184e9005947efe9b206f4736f51144fc9cf25a05
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-08 21:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // add solution here
+var musicians = []
+var instruments = []
 
 function theBeatlesPlay ()
 
+var musicians = []
+
+for (let i = 0; i < selectObject.options.length; i++)
+  for 
+  
 var musicians, instruments;
 
 musicians = ["John Lennon", "Paul McCartney," "Ringo Starr," "George Harrison"];
