85173b4645af98d8ac2e11409632937e1db118e9
Jordan Carroll <jordancarroll1994@gmail.com>
2020-10-08 23:03:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments) {
 
 console.log(theBeatlesPlay(musicians, instruments));
 
-js const facts = []
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
 function johnLennonFacts() {
   
 }
